aaa8ac9088630c80d5258345736852bfabd209f6
Crisktina <crishidalgolopez@gmail.com>
2023-02-23 13:26:57 +0100

    boton

--- src/pages/Bienvenida.jsx
+++ src/pages/Bienvenida.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Boton from "../components/botton/Boton";
 import Buscador from "../components/buscador/Buscador";
 import Footer from "../components/footer/Footer";
 
@@ -6,7 +7,7 @@ function Bienvenida() {
   return (
     <div>
       <Buscador />
-
+      <Boton />
       <h1>Bienvenida</h1>
       <Footer />
     </div>
