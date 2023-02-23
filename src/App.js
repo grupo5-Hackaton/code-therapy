e4635729e295015a507b58209b7975b0a386886d
Crisktina <crishidalgolopez@gmail.com>
2023-02-24 00:27:41 +0100

    rutas App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Clases from "./pages/Clases/Clases";
 import Bienvenida from "./pages/Bienvenida/Bienvenida";
-import Perfil from "./pages/Perfil";
 import DetalleClases from "./pages/DetalleClase";
 import Layout from "./Layout/Layout";
-import ClaseEnDetalle from "./pages/ClaseEnDetalle/ClaseEnDetalle"
-// import axios from "axios";
+import ClaseEnDetalle from "./pages/ClaseEnDetalle/ClaseEnDetalle";
 import PageNotFound from "./pages/PageNotFound/PageNotFound";
 
 function App() {
@@ -13,20 +11,17 @@ function App() {
     <>
       <BrowserRouter>
         <Routes>
-        <Route element={<Layout/>}>
-          <Route path="/" element={<Bienvenida />} />
-            
-          <Route path="/Perfil" element={<Perfil />} />
+          <Route element={<Layout />}>
+            <Route path="/" element={<Bienvenida />} />
 
-          <Route path="/Clases" element={<Clases />} />
+            <Route path="/Clases" element={<Clases />} />
 
-          <Route path="/DetalleClase/:claseid" element={<DetalleClases />} />
+            <Route path="/DetalleClase/:claseid" element={<DetalleClases />} />
 
-          <Route path="/clase-en-detalle" element={<ClaseEnDetalle />} />
-      
-          <Route path="*" element={<PageNotFound/>} />
+            <Route path="/clase-en-detalle" element={<ClaseEnDetalle />} />
 
-        </Route>  
+            <Route path="*" element={<PageNotFound />} />
+          </Route>
         </Routes>
       </BrowserRouter>
     </>
