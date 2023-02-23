7194b2096cc3b8beb59aeff61841867b3dfa384e
Crisktina <crishidalgolopez@gmail.com>
2023-02-23 12:59:12 +0100

    buscador css

--- src/components/buscador/Buscador.jsx
+++ src/components/buscador/Buscador.jsx
@@ -1,9 +1,19 @@
 import React from "react";
+import "./Buscador.css";
+import "../../index.css";
+import lupa from "../../assets/lupa.svg";
 
 function Buscador() {
   return (
     <>
-      <input></input>
+      <div className="buscador-box">
+        <img className="input-icon" src={lupa} alt="Icono Lupa" />
+        <input
+          type="search"
+          className="input-buscador"
+          placeholder="Busca clases por lenguaje"
+        ></input>
+      </div>
     </>
   );
 }
