4e64c1dd9cef4b70ba206994fd07b4f01843f55a
Crisktina <crishidalgolopez@gmail.com>
2023-02-23 11:58:17 +0100

    merge con rama bootstrap

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import logo from "../assets/logo.svg";
+import logo from "../../assets/logo.svg";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
