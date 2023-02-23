312874d98c6a81c92cdd9579e02c94f51e3d6f50
Crisktina <crishidalgolopez@gmail.com>
2023-02-23 13:40:41 +0100

    arreglar llamada a componentes

--- src/pages/Bienvenida.jsx
+++ src/pages/Bienvenida.jsx
@@ -1,20 +1,15 @@
 import React from "react";
-import Buscador from "../components/Buscador";
-import Header from "../components/Header";
 import { Link } from "react-router-dom";
 
 function Bienvenida() {
   return (
     <div>
-      <Buscador />
       <Link to={"/Perfil"}>
         <h1>Aqui se va a Perfil</h1>
       </Link>
       <Link to={"/Clases"}>
         <h1>Aqui se va a Clases</h1>
       </Link>
-      <h1>Bienvenida</h1>
-      <Footer />
     </div>
   );
 }
